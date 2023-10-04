e7d2f07d4d1defa12ef6c4786b1f567dd546b4d3
Heloise POMPIDOU <heloise.pompidou@outlook.fr>
2023-10-04 16:18:50 +0200

    implementation de pid_creat qui alloue les ressources pour pid, suppression de utils qui actuellement ne sert a rien

--- bonus/pipex_bonus.c
+++ bonus/pipex_bonus.c
@@ -6,16 +6,29 @@
 /*   By: hepompid <hepompid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 14:46:55 by hepompid          #+#    #+#             */
-/*   Updated: 2023/10/04 16:04:12 by hepompid         ###   ########.fr       */
+/*   Updated: 2023/10/04 16:15:59 by hepompid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_bonus.h"
 
+int	*pid_creat(int nofcommands, int **fd)
+{
+	int	*pid;
+
+	pid = malloc(nofcommands * sizeof(int));
+	if (!pid)
+	{
+		free_fd(fd, nofcommands);
+		exit(0);
+	}
+	return (pid);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	int	**fd;
-	// int	*pid;
+	int	*pid;
 	int	nofcommands;
 
 	(void)envp;
@@ -25,17 +38,9 @@ int	main(int argc, char **argv, char **envp)
 	nofcommands = argc - 3;
 	if (ft_strncmp(argv[1], "here_doc", 8) == 0)
 		nofcommands = argc - 4;
-	printf("nofcommands = %d\n", nofcommands);
 	fd = fd_creat(nofcommands);
+	pid = pid_creat(nofcommands, fd);
 	fd = fd_init(argc, argv, fd, nofcommands);
-
-	int i = 0;
-	while (i < nofcommands)
-	{
-		printf("fd[%d][0] = %d, fd[%d][1] = %d\n", i, fd[i][0], i, fd[i][1]);
-		i++;
-	}
-	
 	close_all_fd(fd, nofcommands);
 	free_fd(fd, nofcommands);
 	return (0);
